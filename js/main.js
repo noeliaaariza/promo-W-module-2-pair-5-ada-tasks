1050e5e85863227cca5e86444586222369a01c5c
noeliaariza <noelia.ariza@gmail.com>
2024-02-26 11:29:09 +0100

    added dynamic list & some styles

--- js/main.js
+++ js/main.js
@@ -1,1 +1,30 @@
 "use strict";
+
+const tasks = [
+  {
+    name: "Recoger setas en el campo",
+    completed: true,
+  },
+  {
+    name: "Comprar pilas",
+    completed: true,
+  },
+  {
+    name: "Poner una lavadora de blancos",
+    completed: true,
+  },
+  {
+    name: "Aprender cómo se realizan las peticiones al servidor en JavaScript",
+    completed: false,
+  },
+];
+
+const listTasks = document.querySelector(".list");
+
+for (let i = 0; i < tasks.length; i++) {
+  listTasks.innerHTML += `
+  <li class="elements">
+    <input type="checkbox">
+    <span>${tasks[i].name}</span>
+  </li>`;
+}
