47606532069cd85a47e601b8fed96fb46ef748a2
valentinalupla <valentinalunaplaategrafica@gmail.com>
2024-02-27 10:52:11 +0100

    check

--- js/main.js
+++ js/main.js
@@ -15,7 +15,7 @@ const tasks = [
   },
   {
     name: "Aprender cómo se realizan las peticiones al servidor en JavaScript",
-    completed: false,
+    completed: true,
   },
 ];
 
@@ -24,8 +24,31 @@ const listTasks = document.querySelector(".list");
 for (let i = 0; i < tasks.length; i++) {
   listTasks.innerHTML += `
   <li class="elements">
-    <input type="checkbox">
-    <span>${tasks[i].name}</span>
+    <input class="js-check" type="checkbox">
+    <span class="js-tick">${tasks[i].name}</span>
   </li>
   `;
 }
+
+const tick = document.querySelector('.js-tick');
+const check = document.querySelector('.js-check');
+
+function handleclick(){
+
+for(task of tasks){
+  if(tasks.completed === true){
+      tick.classList.add('crossOut');
+    }
+}
+  // for(let i = 0; i < tasks.length; i++) 
+  // if(tasks.completed === true){
+  //   tick.classList.add('crossOut');
+  // }
+}
+
+
+
+
+
+
+check.addEventListener('click', handleclick);
