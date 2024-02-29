1eee6edf40a0a397d91fcbf7e7ad5df0dd48b8eb
valentinalupla <valentinalunaplaategrafica@gmail.com>
2024-02-29 11:15:10 +0100

    add new task

--- js/main.js
+++ js/main.js
@@ -8,13 +8,11 @@ const tasks = [];
 const listTasks = document.querySelector(".list");
 const spans = document.querySelectorAll(".js-span");
 const checks = document.querySelectorAll(".js-check");
+const inputText = document.querySelector('.js-input');
+const inputButton = document.querySelector('.js-add');
 
-fetch(SERVER_URL)
-  .then((response) => response.json())
-  .then((data) => {
-    const tasksData = data.results;
-    renderTasks(tasksData);
-  });
+
+
 
 function renderTasks(tareas) {
   for (const tarea of tareas) {
@@ -26,5 +24,36 @@ function renderTasks(tareas) {
       `;
   }
 }
+
+
+fetch(SERVER_URL)
+  .then((response) => response.json())
+  .then((data) => {
+    const tasksData = data.results;
+    renderTasks(tasksData);
+  });
+
+  function handleNewTask(event){
+    event.preventDefault();
+    const textValue = inputText.value;
+    const newTask = {
+      name: inputText.value, 
+      completed: false,
+    };
+    
+    tasks.push(newTask);
+    for(const newTask of tasks){
+      listTasks.innerHTML += `
+      <li class="elements">
+        <input class="js-check" type="checkbox">
+        <span class="js-span">${newTask.name} </span>
+      </li>
+      `;
+    }
+ 
+
+  }
+
+  inputButton.addEventListener('click', handleNewTask);
 
 // check.addEventListener('click', handleclick);
