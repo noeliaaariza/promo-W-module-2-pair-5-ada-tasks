b1a92507c6e7f365638c25944fc797ed3a8e202c
noeliaariza <noelia.ariza@gmail.com>
2024-03-01 11:21:37 +0100

    Advanced DOM exercise

--- js/main.js
+++ js/main.js
@@ -8,63 +8,79 @@ const tasks = [];
 const listTasks = document.querySelector(".list");
 const spans = document.querySelectorAll(".js-span");
 const checks = document.querySelectorAll(".js-check");
-const inputText = document.querySelector('.js-input');
-const inputButton = document.querySelector('.js-add');
-
-
-
+const inputText = document.querySelector(".js-input");
+const inputButton = document.querySelector(".js-add");
 
 function renderTasks(tareas) {
+  listTasks.innerHTML = "";
   for (const tarea of tareas) {
-    listTasks.innerHTML += `
+    const liElement = document.createElement("li");
+    liElement.setAttribute("class", "elements");
+    listTasks.appendChild(liElement);
+
+    const inputElement = document.createElement("input");
+    inputElement.setAttribute("class", "js-check");
+    inputElement.setAttribute("type", "checkbox");
+    liElement.appendChild(inputElement);
+
+    const spanElement = document.createElement("span");
+    spanElement.setAttribute("class", "js-span");
+    liElement.appendChild(spanElement);
+
+    const textElement = document.createTextNode(tarea.name);
+    spanElement.appendChild(textElement);
+
+    /* listTasks.innerHTML += `
       <li class="elements">
         <input class="js-check" type="checkbox">
         <span class="js-span">${tarea.name} </span>
       </li>
       `;
+    */
   }
 }
-
 
 fetch(SERVER_URL)
   .then((response) => response.json())
   .then((data) => {
     const tasksData = data.results;
-    localStorage.setItem('taskOfDevil', JSON.stringify(tasksData));
-    renderTasks(tasksData);
-    
+    localStorage.setItem("taskOfDevil", JSON.stringify(tasksData));
+    //renderTasks(tasksData);
   });
-  
-  const tasksLocalStorage = JSON.parse(localStorage.getItem('taskOfDevil'));
-  if (tasksLocalStorage !== null) {
-    renderTasks(tasksLocalStorage);
-  } else {
-   renderTasks();
+
+const tasksLocalStorage = JSON.parse(localStorage.getItem("taskOfDevil"));
+if (tasksLocalStorage !== null) {
+  renderTasks(tasksLocalStorage);
+} else {
+  renderTasks();
+}
+
+function handleNewTask(event) {
+  event.preventDefault();
+  const newTask = {
+    name: inputText.value,
+    completed: false,
+  };
+
+  tasks.push(newTask);
+
+  for (const newTask of tasks) {
+    const liElement = document.createElement("li");
+    liElement.setAttribute("class", "elements");
+    listTasks.appendChild(liElement);
+
+    const inputElement = document.createElement("input");
+    inputElement.setAttribute("class", "js-check");
+    inputElement.setAttribute("type", "checkbox");
+    liElement.appendChild(inputElement);
+
+    const spanElement = document.createElement("span");
+    spanElement.setAttribute("class", "js-span");
+    liElement.appendChild(spanElement);
+
+    const textElement = document.createTextNode(newTask.name);
+    spanElement.appendChild(textElement);
   }
-  
+}
 
-  function handleNewTask(event){
-    event.preventDefault();
-    const textValue = inputText.value;
-    const newTask = {
-      name: inputText.value, 
-      completed: false,
-    };
-
-    
-    tasks.push(newTask);
-    for(const newTask of tasks){
-      listTasks.innerHTML += `
-      <li class="elements">
-        <input class="js-check" type="checkbox">
-        <span class="js-span">${newTask.name} </span>
-      </li>
-      `;
-    }
- 
-
-  }
-
-  inputButton.addEventListener('click', handleNewTask);
-
-// check.addEventListener('click', handleclick);
+inputButton.addEventListener("click", handleNewTask);
