b878c5835266a3b003707cd6b47670f457d1675b
valentinalupla <valentinalunaplaategrafica@gmail.com>
2024-03-04 11:12:37 +0100

    final commit

--- js/main.js
+++ js/main.js
@@ -3,7 +3,8 @@
 const GITHUB_USER = "noeliaaariza";
 const SERVER_URL = `https://dev.adalab.es/api/todo/${GITHUB_USER}`;
 
-const tasks = [];
+// Esta es una variable para la nueva tarea
+let tasks = [];
 
 const listTasks = document.querySelector(".list");
 const spans = document.querySelectorAll(".js-span");
@@ -30,22 +31,16 @@ function renderTasks(tareas) {
     const textElement = document.createTextNode(tarea.name);
     spanElement.appendChild(textElement);
 
-    /* listTasks.innerHTML += `
-      <li class="elements">
-        <input class="js-check" type="checkbox">
-        <span class="js-span">${tarea.name} </span>
-      </li>
-      `;
-    */
   }
 }
+
 
 fetch(SERVER_URL)
   .then((response) => response.json())
   .then((data) => {
     const tasksData = data.results;
+    tasks = tasksData;
     localStorage.setItem("taskOfDevil", JSON.stringify(tasksData));
-    //renderTasks(tasksData);
   });
 
 const tasksLocalStorage = JSON.parse(localStorage.getItem("taskOfDevil"));
@@ -63,24 +58,8 @@ function handleNewTask(event) {
   };
 
   tasks.push(newTask);
-
-  for (const newTask of tasks) {
-    const liElement = document.createElement("li");
-    liElement.setAttribute("class", "elements");
-    listTasks.appendChild(liElement);
-
-    const inputElement = document.createElement("input");
-    inputElement.setAttribute("class", "js-check");
-    inputElement.setAttribute("type", "checkbox");
-    liElement.appendChild(inputElement);
-
-    const spanElement = document.createElement("span");
-    spanElement.setAttribute("class", "js-span");
-    liElement.appendChild(spanElement);
-
-    const textElement = document.createTextNode(newTask.name);
-    spanElement.appendChild(textElement);
-  }
+  console.log(tasks);
+renderTasks(tasks);
 }
 
 inputButton.addEventListener("click", handleNewTask);
